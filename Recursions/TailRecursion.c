061c2aa904fa288418811656ab59672ea37f6ff1
udaykumar-2006 <nallaniuday1504@gmail.com>
2025-06-14 13:01:52 +0530

    Update TailRecursion.c

--- Recursions/TailRecursion.c
+++ Recursions/TailRecursion.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+
 void tailRec(int n) {
-    if(n == 0) return;
+    if (n == 0) return;
     printf("%d ", n);
-    tailRec(n - 1);
+    tailRec(n - 1);  
+}
+
+int main() {
+    tailRec(5);
+    return 0;
 }

a82b7ebb3c9243cec6de64464e81505c275f39c0
udaykumar-2006 <nallaniuday1504@gmail.com>
2025-06-14 13:02:23 +0530

    Update HeadRecursion.c

--- Recursions/HeadRecursion.c
+++ Recursions/HeadRecursion.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+
 void headRec(int n) {
-    if(n == 0) return;
-    headRec(n - 1);
-    printf("%d ", n);
+    if (n == 0) return;
+    headRec(n - 1);   
+    printf("%d ", n); 
+}
+
+int main() {
+    headRec(5);
+    return 0;
 }

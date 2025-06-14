1b506defa333f6305f851c6118e6ab8241856d76
udaykumar-2006 <nallaniuday1504@gmail.com>
2025-06-14 13:03:07 +0530

    Update TreeRecursion.c

--- Recursions/TreeRecursion.c
+++ Recursions/TreeRecursion.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
 void treeRec(int n) {
-    if(n == 0) return;
+    if (n == 0) return;
     printf("%d ", n);
-    treeRec(n - 1);
-    treeRec(n - 1);
+    treeRec(n - 1);   // Firstrecursive call
+    treeRec(n - 1);   // Secondrecursive call
+}
+
+int main() {
+    treeRec(3);
+    return 0;
 }
